cda46223651022f5879bb8e357b1392fa349f4e4
ashutosh-shukla <121450702+ashutosh-shukla@users.noreply.github.com>
2024-08-22 09:36:10 +0530

    Update panel.py

--- panel.py
+++ panel.py
@@ -17,8 +17,9 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"C:\Users\heman\OneDrive\Desktop\panel")
-    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+    #os.chdir(r"C:\Users\heman\OneDrive\Desktop\panel")
+    #df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+    continue:
 
 
 
